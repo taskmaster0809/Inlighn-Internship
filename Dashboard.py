d4cb0b5c13966b67a5f834c272697e7e15259e68
taskmaster0809 <88977761+taskmaster0809@users.noreply.github.com>
2025-11-21 20:14:41 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,11 +1,11 @@
-import streamlit.components.v1 as components
+import streamlit as st
 
 def dashboard_page():
     st.title("E-Commerce Dashboard")
 
     power_bi_url = "https://app.powerbi.com/view?r=eyJrIjoiNWJlZmE2ZjYtYTQwYi00ZDcxLTkzOGMtYTIwNTFmNWY0MWNlIiwidCI6IjMxY2MwODdiLWJhNDQtNGMyOS1hNmRkLWZkZjYwMDk0ZjNiZiJ9"
 
-    components.html(
+    st.markdown(
         f"""
         <iframe 
             src="{power_bi_url}" 

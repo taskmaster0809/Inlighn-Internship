a1a2ed145ae0a9887b0a8ca6771a5b6208545e02
taskmaster0809 <88977761+taskmaster0809@users.noreply.github.com>
2025-11-21 19:56:48 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,17 +1,16 @@
 def dashboard_page():
     st.title("E-Commerce Dashboard")
-    # st.write(f"Welcome **{st.session_state['current_user']}**! Air Quality Index Dashboard!")
-
+    
     # Power BI Embed URL
-    power_bi_url = "https://app.powerbi.com/groups/me/reports/6e8ae584-e2a0-4594-8f93-2c8f72faede9?experience=power-bi"
+    power_bi_url = "https://app.powerbi.com/view?r=eyJrIjoiNWJlZmE2ZjYtYTQwYi00ZDcxLTkzOGMtYTIwNTFmNWY0MWNlIiwidCI6IjMxY2MwODdiLWJhNDQtNGMyOS1hNmRkLWZkZjYwMDk0ZjNiZiJ9"
 
     # Embed Power BI Dashboard using iframe
     st.markdown(
         f"""
         <iframe 
             src="{power_bi_url}" 
-            width="100%" 
-            height="600" 
+            width="600" 
+            height="373.5" 
             frameborder="0" 
             allowfullscreen="true">
         </iframe>
